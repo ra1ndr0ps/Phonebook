bd08ffef344f929ffed64ec1ee324871a2e13de5
ra1ndr0ps <>
2020-08-11 17:51:33 +0300

    xyz

---
+++ backend/build/precache-manifest.a81c572ed5735f0c343430f4d5712964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb639de8d4ddd6dc44bd267ee91e246a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a458086d5f52915169a0",
+    "url": "/static/js/main.b6ffc7e5.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(morgan(function (tokens, req, res) {
 }))
 
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
       {
