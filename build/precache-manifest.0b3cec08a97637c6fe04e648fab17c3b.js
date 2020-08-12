750c8ea115dd45139a1d515480c62d0de3ed6108
ra1ndr0ps <>
2020-08-12 17:03:31 +0300

    update

---
+++ build/precache-manifest.0b3cec08a97637c6fe04e648fab17c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68e83b9c2352b96e0f15f28bff920d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f365bf701c56f840f69",
+    "url": "/static/js/main.a4a1c146.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
