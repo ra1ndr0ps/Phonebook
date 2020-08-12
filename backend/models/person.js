72c583985ede3ebd56a8ab56ebab73bc1a136a1d
ra1ndr0ps <>
2020-08-12 18:35:02 +0300

    xyz

--- backend/models/person.js
+++ backend/models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
+const url = "mongodb+srv://rain:learnreact@cluster0.xmpu9.mongodb.net/phonebook-app?retryWrites=true&w=majority"
 
 console.log('connecting to', url)
 
