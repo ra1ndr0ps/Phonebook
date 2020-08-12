4373253bfeaef87ecbb4c839198c93a0698e71fc
ra1ndr0ps <>
2020-08-12 16:12:46 +0300

    update

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,8 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
-
 app.use(express.json())
+app.use(express.static('build'))
 
 app.use(morgan(function (tokens, req, res) {
   return [
